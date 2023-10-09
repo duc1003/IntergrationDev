b92ad8d5847b0dce6f26bcd2dedbbd5f992ae503
ducc0506 <ducnd.ntd.vn@gmail.com>
2023-10-09 09:00:22 +0700

    tuan 4

---
+++ bai 1/client.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 8080
+s.connect((host, port))
+str = "Thong tin tu client"
+s.send(str.encode('utf-8'))
+print("Nhan lai thong tin tu server: ", s.recv(1024))
+s.close()
